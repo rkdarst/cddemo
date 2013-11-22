8cf82f926d280dc304430a03d4ad5fa74fa0522d
Richard Darst <rkd@zgib.net>
2013-11-22 11:48:59 +0200

    Updates to all files.  Add preliminary instructions files.

--- Makefile
+++ Makefile
@@ -1,6 +1,13 @@
 default:
 	echo "Select an option from the file to run"
 
-webgraph:
+get_webgraph:
 	wget http://www3.nd.edu/~networks/resources/www/www.dat.gz
 	gunzip www.dat.gz
+
+DEST=amor:.www/cddemo/
+copy_files:
+	scp www-normalized.dat $(DEST)
+	scp webgraph-S.p* $(DEST)
+
+
--- karate.py
+++ karate.py
@@ -4,8 +4,11 @@ import networkx
 import numpy.linalg
 
 def laplacian_matrix(g, nodes):
-    # We should use networkx.laplacian_matrix, but for instructional
-    # value we will do this manually.
+    """Compute laplacian matrix.
+
+    We should use networkx.laplacian_matrix, but for instructional
+    value we will do this manually.
+    """
     L = numpy.zeros(shape=(len(nodes), len(nodes)))
     for n1 in nodes:
         # For each node, for each neighbor, place a -1 at each element
@@ -16,9 +19,20 @@ def laplacian_matrix(g, nodes):
         L[n1, n1] = g.degree(n1)
     return L
 
+def print_group(l):
+    """Function used for printing nodes in a group.
+
+    You could also simply do:
+      print ' '.join(str(n) for n in l)
+    """
+    for n in l:
+        print n,
+    print
 
 # Get karate club graph from networkx.
 g = networkx.karate_club_graph()
+#networkx.write_edgelist(g, 'karate.txt', data=False)
+
 
 nodes = g.nodes()
 L = laplacian_matrix(g, nodes)
@@ -28,34 +42,71 @@ ev, evec = numpy.linalg.eigh(L)
 # Sort by eigenvalue
 ev_ranks = numpy.argsort(ev)
 
+# Do a clustering based on positive/negative Laplacian values:
+print "Laplacian based clustering:"
 print "should be zero:", ev[ev_ranks[0]]
 print "next eigenvalue:", ev[ev_ranks[1]]
 evec1 = evec[:,ev_ranks[1]]
 
 group1 = [ nodes[i] for i in range(len(evec1)) if evec1[i] >= 0 ]
 group2 = [ nodes[i] for i in range(len(evec1)) if evec1[i] < 0 ]
-print "Group sizes: group1, group2:", len(group1), len(group2)
+print "Group sizes, laplacian positive/negative split:", len(group1), len(group2)
+#
 
 # Do a check to ensure that all nodes were partitioned
 assert set(group1) | set(group2) == set(g.nodes())
-
+# Compute the boundary edges:
 boundary_edges = networkx.edge_boundary(g, group1)
-# This should be reversible
+print "Laplacian cut size is:", len(boundary_edges)
+# This should be reversible - boundary of group1 should equal boundary
+# of group 2.  Do a test, to ensure that we used our tools correctly.
 assert len(boundary_edges) == len(networkx.edge_boundary(g, group2))
+print "group1:",
+print_group(group1)
+print "group2:",
+print_group(group2)
 
 
-print "Cut size is", len(boundary_edges)
+# Do a clustering where the lowest 16 eigenvalues are in one cluster,
+# and then below, a clustering where the lowest 18 eigenvalues are in
+# one cluster.
+ranks = numpy.argsort(evec1)
+group1 = set(nodes[i] for i in ranks[:16])
+group2 = set(nodes[i] for i in ranks[16:])
+boundary_edges = networkx.edge_boundary(g, group1)
+print "Group sizes, (16 lowest):", len(group1), len(group2)
+print "Cut size (16 lowest) is:", len(boundary_edges)
+print "group1:",
+print_group(group1)
+print "group2:",
+print_group(group2)
+print
 
+group1 = set(nodes[i] for i in ranks[:18])
+group2 = set(nodes[i] for i in ranks[18:])
+boundary_edges = networkx.edge_boundary(g, group1)
+print "Group sizes, (18 lowest):", len(group1), len(group2)
+print "Cut size (18 lowest) is:", len(boundary_edges)
+print "group1:",
+print_group(group1)
+print "group2:",
+print_group(group2)
+print
+
+
+
+
+#
+# Part 2: Newman algorithm for modularity clustering.
+#
 
 
 import pcd.graphs
 g = pcd.graphs.karate_club()
-#g = pcd.graphs.dolphins()
-#g = pcd.graphs.football()
 nodes = g.nodes()
-#from fitz import interactnow
 
 def shuffled(l):
+    """Make a shuffled version of a list."""
     l = list(l)
     import random
     random.shuffle(l)
@@ -112,7 +163,10 @@ def modularity(g, comms):
         Q += E_in/(M*1) - (K_in/(2*M))**2
     return Q
 def modularity2(g, comms):
-    """Compute modularity: node-centric version"""
+    """Compute modularity: node-centric version.
+
+    We implement Modularity computation twice to be able to check our
+    work."""
     Q = 0.0
     M = float(g.number_of_edges())
     for n1 in g.nodes_iter():
@@ -137,9 +191,11 @@ for i, n in enumerate(nodes):
     node_comms[n] = i
 
 
+# This is the actual algorithm.
 last_Q = modularity(g, comms)
 best_Q = last_Q
 best_comms = copy.deepcopy(comms)
+print "# Output is: number_of_communities, Q, dQ"
 while True:
     best_dQ = -1e9
     best_comm = None
@@ -180,7 +236,7 @@ while True:
     # actual computed modularity change.
     assert abs(Q-last_Q-best_dQ) < 1e-10
 
-    print Q, best_dQ, len(comms)
+    print len(comms), Q, best_dQ
     if Q > best_Q:
         best_Q = Q
         best_comms = copy.deepcopy(comms)
@@ -190,16 +246,24 @@ while True:
 
     last_Q = Q
 
+print "Results at the maximal modularity level:"
 print "Optimal modularity:", best_Q
 print "Optimal number of communities:", len(best_comms)
 print "Optimal communities:", best_comms
+print
+
+# What is the results at the the two-community level of the
+# algorithm:
+
+print "Results at the level where there are two communities:"
+print "Optimal modularity:", modularity(g, comms_2)
+print "Optimal number of communities:", len(comms_2)
 
 group1, group2 = comms_2.values()
-#print group1
-#print group2
-
-print comms_2
-print modularity(g, comms_2)
+print "group 1:",
+print_group(group1)
+print "group 2:",
+print_group(group2)
 
 boundary_edges = networkx.edge_boundary(g, group1)
-print "Cut, Newman algorithm: ", len(boundary_edges)
+print "Cut size:", len(boundary_edges)
